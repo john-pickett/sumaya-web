561d252e104726fa362dcf28094f9959789fe8d4
John Pickett <john.pickett@gmail.com>
2026-04-11 11:31:39 -0500

    Updated links

--- src/config/links.ts
+++ src/config/links.ts
@@ -1,6 +1,6 @@
 // TODO: Replace with actual App Store and Play Store URLs when available
-export const APP_STORE_URL = "TODO_APP_STORE_URL";
-export const PLAY_STORE_URL = "TODO_PLAY_STORE_URL";
+export const APP_STORE_URL = "https://apps.apple.com/us/app/sumaya-meditation-timer/id6748861153";
+export const PLAY_STORE_URL = "https://play.google.com/store/apps/details?id=com.fiveamsoftware.sumaya&hl=en_US";
 
 export const SITE_URL =
-  process.env.NEXT_PUBLIC_SITE_URL ?? "https://sumayaapp.com";
+  process.env.NEXT_PUBLIC_SITE_URL ?? "https://getsumaya.com";
